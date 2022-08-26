6f479b16ab2a348e556fc108a3891f312d2239e6
cpz <214564605@qq.com>
2022-08-27 00:15:17 +0800

    优化编辑 添加功能 分页等

--- src/pages/admin/admin_user/index.jsx
+++ src/pages/admin/admin_user/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input,Table ,Space,Tooltip} from 'antd';
+import { Button, Input,Table ,Pagination} from 'antd';
 import React, {useEffect} from 'react';
 import axios from "axios"
 import {nanoid} from "nanoid"
@@ -6,13 +6,16 @@ import {set_menu, set_menu_tier} from "../../../store/admin/menu_data";
 import {Option} from "antd/es/mentions";
 import "../../../components/admin/css/base.css";
 import "../css/base.css";
-import {CloseOutlined} from "@ant-design/icons";
+import {CloseOutlined} from "@ant-design/icons";//引入图标
 
 //引入admin 管理的基础样式文件
 class Index extends React.Component {
-
     state = {
-        page_data: [],//分页数据
+        page_data: {
+            current_page:1,//当前分页
+            per_page:'20', //默认分页大小
+            pageSizeOptions:[10,15,20,30,50,100]//分页选项
+        },//分页数据
         table_loading:false,//页面数据是否加载中
         //编辑数据
         modify_page:{
@@ -24,10 +27,6 @@ class Index extends React.Component {
         },
     };
     columns = this.table_columns();
-    constructor(props){
-        super(props)
-
-    }
 
     //获取参数 没有时获取默认值
     get_params(name,val){
@@ -68,7 +67,7 @@ class Index extends React.Component {
     //添加数据
     add(){
         this.state.modify_page.show=true
-        this.state.modify_page.edit_type='edit'
+        this.state.modify_page.edit_type='add'
         this.setState(this.state);//刷新页面
     }
     //编辑数据
@@ -177,6 +176,149 @@ class Index extends React.Component {
         let submit_title='';
         if(data.edit_type in  data.title_map)title=data.title_map[data.edit_type];
         if(data.edit_type in  data.submit_title_map)submit_title=data.submit_title_map[data.edit_type];
+        
+        //添加的页面
+        let add_jsx=<>
+            {/*提交类型 表示当前表单的操作类型*/}
+            <input type="hidden" name="action_type" value={data.edit_type}/>
+            {/*账号名称*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">账号名称</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入账号名称" name="account_name" />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}} className="form_item_error">账号名称</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入账号密码</div>
+            </div>
+            {/*密码*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">密码</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入密码" name="password" type="password" />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入密码</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入密码</div>
+            </div>
+            {/*手机号*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">手机号</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入手机号" name="phone" />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入手机号</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入手机号</div>
+            </div>
+            {/*用户名称*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">用户名称</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入真实名称" name="user_name" />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入用户名称</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入用户名称</div>
+            </div>
+            {/*真实名称*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">真实名称</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入真实名称" name="real_name" />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入真实名称</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入真实名称</div>
+            </div>
+            <div className="form_item">
+                <div className="item_col_25"></div>
+                <div className="form_item_col item_col_40">
+                    <Button type="primary" htmlType="submit">{submit_title}</Button>
+                </div>
+            </div>
+        </>
+        //编辑的页面
+        let edit_jsx=<>
+            {/*提交类型 表示当前表单的操作类型*/}
+            <input type="hidden" name="action_type" value={data.edit_type}/>
+            {/*ID主键*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">ID</div>
+                <div className="form_item_col item_col_40">
+                    <label>{data.default_data.id}</label>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}></div>
+            </div>
+
+            {/*账号名称*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">账号名称</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入账号名称" name="account_name" defaultValue={data.default_data.account_name} />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}} className="form_item_error">账号名称</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入账号密码</div>
+            </div>
+            {/*密码*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">密码</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入密码" name="password" type="password" />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入密码</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入密码</div>
+            </div>
+            {/*手机号*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">手机号</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入手机号" name="phone" defaultValue={data.default_data.phone} />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入手机号</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入手机号</div>
+            </div>
+            {/*用户名称*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">用户名称</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入真实名称" name="user_name"  defaultValue={data.default_data.user_name}/>
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入用户名称</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入用户名称</div>
+            </div>
+            {/*真实名称*/}
+            <div className="form_item">
+                <div className="form_item_label item_col_25">真实名称</div>
+                <div className="form_item_col item_col_40">
+                    <Input placeholder="请输入真实名称" name="real_name" defaultValue={data.default_data.real_name} />
+                    {/*错误提示*/}
+                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入真实名称</div>
+                </div>
+                {/*普通提示*/}
+                <div style={{marginLeft:"10px"}}>请输入真实名称</div>
+            </div>
+            <div className="form_item">
+                <div className="item_col_25"></div>
+                <div className="form_item_col item_col_40">
+                    <Button type="primary" htmlType="submit">{submit_title}</Button>
+                </div>
+            </div>
+        </>
 
         return <>
             <div className={"page_edit"+" "+(data.show?'':"hide")}>
@@ -187,69 +329,8 @@ class Index extends React.Component {
                     </div>
                     <div style={{"marginTop":"20px"}}>
                         <form method="post">
-                            {/*提交类型 表示当前表单的操作类型*/}
-                            <input type="hidden" name="action_type" value={data.edit_type}/>
-                            {/*账号名称*/}
-                            <div className="form_item">
-                                <div className="form_item_label item_col_25">账号名称</div>
-                                <div className="form_item_col item_col_40">
-                                    <Input placeholder="请输入账号名称" name="account_name" />
-                                    {/*错误提示*/}
-                                    <div role="alert" style={{display:"none"}} className="form_item_error">账号名称</div>
-                                </div>
-                                {/*普通提示*/}
-                                <div style={{marginLeft:"10px"}}>请输入账号密码</div>
-                            </div>
-                            {/*密码*/}
-                            <div className="form_item">
-                                <div className="form_item_label item_col_25">密码</div>
-                                <div className="form_item_col item_col_40">
-                                    <Input placeholder="请输入密码" name="password" type="password" />
-                                    {/*错误提示*/}
-                                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入密码</div>
-                                </div>
-                                {/*普通提示*/}
-                                <div style={{marginLeft:"10px"}}>请输入密码</div>
-                            </div>
-                            {/*手机号*/}
-                            <div className="form_item">
-                                <div className="form_item_label item_col_25">手机号</div>
-                                <div className="form_item_col item_col_40">
-                                    <Input placeholder="请输入手机号" name="phone" />
-                                    {/*错误提示*/}
-                                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入手机号</div>
-                                </div>
-                                {/*普通提示*/}
-                                <div style={{marginLeft:"10px"}}>请输入手机号</div>
-                            </div>
-                            {/*用户名称*/}
-                            <div className="form_item">
-                                <div className="form_item_label item_col_25">用户名称</div>
-                                <div className="form_item_col item_col_40">
-                                    <Input placeholder="请输入真实名称" name="user_name" />
-                                    {/*错误提示*/}
-                                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入用户名称</div>
-                                </div>
-                                {/*普通提示*/}
-                                <div style={{marginLeft:"10px"}}>请输入用户名称</div>
-                            </div>
-                            {/*真实名称*/}
-                            <div className="form_item">
-                                <div className="form_item_label item_col_25">真实名称</div>
-                                <div className="form_item_col item_col_40">
-                                    <Input placeholder="请输入真实名称" name="real_name" />
-                                    {/*错误提示*/}
-                                    <div role="alert" style={{display:"none"}}  className="form_item_error">请输入真实名称</div>
-                                </div>
-                                {/*普通提示*/}
-                                <div style={{marginLeft:"10px"}}>请输入真实名称</div>
-                            </div>
-                            <div className="form_item">
-                                <div className="item_col_25"></div>
-                                <div className="form_item_col item_col_40">
-                                    <Button type="primary" htmlType="submit">{submit_title}</Button>
-                                </div>
-                            </div>
+                            {data.edit_type==='add'?add_jsx:''}
+                            {data.edit_type==='edit'?edit_jsx:''}
                         </form>
                     </div>
                 </div>
@@ -260,20 +341,8 @@ class Index extends React.Component {
 
     //页面刷新
     render() {
-        let pagination = {
-            showSizeChanger: true, //是否可以改变 pageSize
-            showQuickJumper: false, //是否可以快速跳转至某页
-            total: this.state.page_data.total,
-            pageSize:this.state.page_data.per_page,
-            current: this.state.page_data.current_page,
-            pageSizeOptions:[10,15,20,30,50,100],
-            //显示总条数
-            showTotal: (total, range) => `显示 ${range[0]}-${range[1]} , 总共 ${total} 条`,
-            //pageSize 变化的回调
-            onShowSizeChange: (current, pageSize) => this.get_page_data(current,pageSize),
-            //页码改变的回调，参数是改变后的页码及每页条数
-            onChange: (current, pageSize) => this.get_page_data(current,pageSize),
-        };
+        let page_data=this.state.page_data
+        
         return (
             <>
                 <div style={{"padding": "10px","position":"relative"}}>
@@ -303,18 +372,36 @@ class Index extends React.Component {
                     <Table rowKey="id"
                         columns={this.columns}
                         dataSource={this.state.page_data.data}
-                        pagination={pagination}
                         position='bottomCenter'
+                        pagination={false}
                         bordered
                         loading={this.state.table_loading}
                         showSizeChanger='false'
                     />
+                    <div className="page_bottom">
+                        <div className="bottom_function">
+                            <a href='#' onClick={()=>this.add()}>添加用户</a>
+                        </div>
+                        <div className='paging_module'>
+                            <Pagination
+                                current={page_data.current_page}
+                                showSizeChanger={true}
+                                showQuickJumper={false}
+                                total={page_data.total}
+                                pageSizeOptions={page_data.pageSizeOptions}
+                                pageSize={page_data.per_page}
+                                showTotal={(total, range) => `显示 ${range[0]}-${range[1]} , 总共 ${total} 条`}
+                                onShowSizeChange={(current, page_size) => this.get_page_data(current,page_size)}
+                                onChange={(current, page_size) => this.get_page_data(current,page_size)}
+                            />
+                        </div>
+                    </div>
+
                 </div>
                 {/*页面数据的修改*/}
                 {this.modify_page()}
             </>
         )
     }
-
 }
 export default Index;
