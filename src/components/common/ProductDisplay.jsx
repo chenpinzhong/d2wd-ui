186fe1411f10689de5e849ba1dc4fbf8a894d10c
cpz <214564605@qq.com>
2022-09-13 17:43:02 +0800

    首页展示优化

--- src/components/common/ProductDisplay.jsx
+++ src/components/common/ProductDisplay.jsx
@@ -1,34 +1,37 @@
 import React from "react"
-import "./css/product_display.css" //引入样式文件
+import "./css/product_display.css"
+
 class ProductDisplay extends React.Component{
-  render(){
-    return(
-      <>
-        <div className="product_display">
-            <div className="product_item mt_40">
-                <figure className="product_thumb">
-                    <a href="/shop/product/show/id/1">
-                        <img className="pri-img" src={process.env.PUBLIC_URL + '/images/product/iphone13 black.jpg'} width="270" height="270" alt="product"/>
-                    </a>
-                    <div className="product_badge">
-                        <div className="product_label new"><span>new</span></div>
-                        <div className="product_label discount"><span>0%</span></div>
-                    </div>
-                </figure>
-                <div className="product_caption">
-                    <p className="product_name">
-                        {/*<!--产品名称-->*/}
-                        <a href="/shop/product/show/id/1">iphone13</a>
-                    </p>
-                    <div className="price_box">
-                        <span className="price_regular">¥5399.00</span>
-                        <span className="price_old"><del>¥6399.00</del></span>
+
+    render(){
+        let product=this.props.product
+        return(
+            <>
+                <div  className="product_display">
+                    <div className="product_item mt_40">
+                        <figure className="product_thumb">
+                            <a href={"/shop/product/show/id/"+product.id}>
+                                <img className="pri-img" src={product.product_image} width="270" height="270" alt="product"/>
+                            </a>
+                            <div className="product_badge">
+                                {product.discount.new===true?(<div className="product_label new"><span>new</span></div>):''}
+                                <div className="product_label discount"><span>{product.discount}%</span></div>
+                            </div>
+                        </figure>
+                        <div className="product_caption">
+                            <p className="product_name">
+                                {/*<!--产品名称-->*/}
+                                <a href={"/shop/product/show/id/"+product.id}>{product.product_name}</a>
+                            </p>
+                            <div className="price_box">
+                                <span className="price_regular">{product.sale_price}</span>
+                                <span className="price_old"><del>{product.price}</del></span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-      </>
-    )
-  }
+            </>
+        )
+    }
 }
 export default ProductDisplay
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -8,13 +8,46 @@ import React, {useEffect,useState} from 'react';
 import Slide from '../../components/common/Slide' //幻灯片组件
 import ProductDisplay from '../../components/common/ProductDisplay'
 import axios from "axios";
-import {message} from "antd"; //产品显示组件
+import {message} from "antd";
+import {nanoid} from "nanoid"; //产品显示组件
 
-
+//渲染产品列表
+function ProductList(props){
+    let product_list=props.product_list;
+    //数组切片
+    const chunk_array = (arr,size) =>{
+        if(arr.length === 0)return [];
+        const tempArr = [];
+        const chunkLen = Math.ceil(arr.length / size);
+        let last = 0;
+        for(let i = 0;i<chunkLen;i++){
+            if(size * i >= arr.length){
+                last = arr.length;
+            }else{
+                last = size * i + size;
+            }
+            tempArr.push(arr.slice(size * i,last));
+        }
+        return tempArr;
+    }
+    let chunk_product_list=chunk_array(product_list, 4);
+    return chunk_product_list.map(function (product_list){
+        return(
+            <div className="row mtn-40" key={nanoid()}>
+                {
+                    product_list.map(function (product){
+                        return <ProductDisplay key={nanoid()} product={product}/>
+                    })
+                }
+            </div>
+        )
+    })
+}
 //用户首页
 function Index(){
     const [home_poster, set_home_poster] = useState(0);
     const [product_list, set_product_list] = useState(0);
+
     useEffect(()=>{
         //第一次渲染时 需要进行菜单列表的请求
         let server_url = process.env.REACT_APP_SERVER_URL;
@@ -26,9 +59,9 @@ function Index(){
                     //首页海报
                     let home_poster=response_data['data']['home_poster'];
                     set_home_poster(home_poster);
-                    console.log('父组件渲染')
                     //首页推荐产品
                     let product_list=response_data['data']['product_list'];
+                    console.log(product_list)
                     set_product_list(product_list);
                 }else{
                     message.info('获取首页信息存在错误!').then(r => console.log('获取首页信息存在错误'))
@@ -40,9 +73,10 @@ function Index(){
             }
         );
     },[])
-
+	
     //拉取产品列表
     return (
+
         <>
             <main>
                 {/*轮播图*/}
@@ -59,12 +93,7 @@ function Index(){
                                 <p>大家喜欢的好物</p>
                             </div>
                             {/*产品展示模块*/}
-                            <div className='row mtn-40'>
-                                <ProductDisplay/>
-                                <ProductDisplay/>
-                                <ProductDisplay/>
-                                <ProductDisplay/>
-                            </div>
+                            <ProductList  product_list={product_list}/>
                         </div>
                     </div>
                 </section>
